80e01cd7d5afc2277e911368ea22a3f864379299
Avii Yadav <ghostgaming0102@gmail.com>
2026-05-15 13:10:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+// var a=76
+// var b=53
+// console.log("a%b = ",a%b)
+// ///arithmetic opeators
+// var c=2
+// var d=3
+// c+=d
+// c-=d
+// c*=d
+// c/=d
+// c%=d
+// c**=d
+// // console.log(c)
+
+// //comparison operators 
+// let e = 55
+// let f = 55
+
+// let x = 73
+// let y = 7
+
+// console.log(e==f || x==y);
+
+// // let _age = 20
+// // if(_age>18){
+// //     console.log("you can drive")
+// // }else{
+// //     console.log("you can't drive")
+// // }
+// // if(_age>20){
+// //     console.log("you can vote")
+// // }else{
+// //     console.log("you can't vote")
+// // }
+
+
+
+// let _marks = 10
+// if(_marks>89){
+//     console.log("Grade A")
+// }else if(_marks<90 && _marks>70){
+//     console.log("Grade B")
+// }else if(_marks<70 && _marks>50){
+//     console.log("Grade C")
+// }else if(_marks<50 && _marks>33){
+//     console.log("Grade D")
+// }else{
+//     console.log("Fail")
+// }
+// // let str = 'Hello';
+// // str.length;              // Get length
+// // str.toUpperCase();       // Convert to uppercase
+// // str.toLowerCase();       // Convert to lowercase
+// // str.charAt(0);          // Get character at index
+// // str.substring(0, 3);    // Extract substring
+// // str.includes('ell');    // Check if contains
+// // str + ' World';         // Concatenate
+// // let str = 'Hello, World!';
+// // let str = "Hello, World!";
+// // let str = `Hello, World!`;
+// // let name = 'Alice';
+// // let greeting = `Hello, ${name}!`; // Supports variables and expressions
+// // let str = new String('Hello, World!');
+// let _height = 175
+// let _weight = 55
+// let _bmi = _weight/(_height/100)**2
+// console.log("BMI: ",_bmi)
+// var _age = 25
+// _age>18 ? console.log("you can gop gop"): console.log("you can't")
+// var _SPACE
+// for(_SPACE = 1;_SPACE <=100;_SPACE++){
+//     console.log(_SPACE)}
+
+// let _s=0
+// while(_s<=70){
+// console.log(_s);
+// _s++
+// }
+// var _t
+// for(_t = 1;_t <=108;_t++){
+//     console.log("Abhay")
+//     console.log("Yadav")
+// }
+
+// let u = 90
+
+// do{
+//     console.log(u,"tinku");
+//     u++;
+// }while(u<10)
+
+let name = 'HARRY';
+console.log(name.replace('HARRY','ALLU ARJUN'))
+
+// name.length
+// console.log(name.length)
+
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+
